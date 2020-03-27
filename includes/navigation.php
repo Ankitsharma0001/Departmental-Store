a094b384295de2f66f3861340560d4ef9c15b64a
Ankitsharma0001 <49328285+Ankitsharma0001@users.noreply.github.com>
2020-03-27 10:46:42 +0530

    Initial commit

---
+++ includes/navigation.php
@@ -0,0 +1,78 @@
+ <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">Departmental Store</a>
+            </div>
+            <!-- Top Menu Items -->
+            
+            <ul class="nav navbar-right top-nav">
+                
+                
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> User <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
+                        </li>
+                        
+                        <li class="divider"></li>
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            
+            
+            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    
+                    <li>
+                        <a href="index.php"><span class="glyphicon glyphicon-dashboard" aria-hidden="true"></span> Dashboard</a>  
+                    </li>
+                    
+                    <li>
+                       <a href="categories.php"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Category</a>
+                    </li>
+                    
+                    <li>
+                       <a href="companies.php"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Companies </a>
+                    </li>
+                    
+                    
+                    <li class="active">
+                        <a href="javascript:;" data-toggle="collapse" data-target="#demo2"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Products <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="demo2" class="collapse">
+                            <li>
+                                <a href="manage_products.php">Manage Products</a>
+                            </li>
+                            <li>
+                                <a href="add_products.php">Add Products</a>
+                            </li>
+                        </ul>
+                        
+                    </li>
+                    
+                    <li class="active">
+                        <a href="blank-page.html"><span class="glyphicon glyphicon-search" aria-hidden="true"></span> Search Product</a>
+                    </li>
+                     <li class="active">
+                        <a href="blank-page,html;"> <span class="glyphicon glyphicon-user" aria-hidden="true"></span>  Customers </a>
+                        
+                    </li>
+                    <li>
+                        <a href="index-rtl.html"><span class="glyphicon glyphicon-dashboard" aria-hidden="true"></span> Reports</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </nav>
+
+       
